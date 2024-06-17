b1193bae9937d3a3e70f4346c65c2732dbe38f34
Thomas Charpentier <thomas.charpentier9@etu.univ-lorraine.fr>
2024-06-17 14:07:05 +0200

    Ajout serveur http

--- ServeurProxyHTTP/Main.java
+++ ServeurProxyHTTP/Main.java
@@ -6,7 +6,8 @@ import com.sun.net.httpserver.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/applications/myapp", new ServeurHTTPHandler());
+        server.createContext("/applications", new IncidentHTTPHandler());
+        server.createContext("/applications", new RestaurantHTTPHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
