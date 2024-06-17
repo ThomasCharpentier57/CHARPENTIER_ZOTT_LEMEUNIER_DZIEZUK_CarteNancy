96366fac76fbd35d2b5f37a4de3e4be0cdb0eb0e
Thomas Charpentier <thomas.charpentier9@etu.univ-lorraine.fr>
2024-06-17 14:21:35 +0200

    Modification

--- ServeurProxyHTTP/Main.java
+++ ServeurProxyHTTP/Main.java
@@ -6,8 +6,8 @@ import com.sun.net.httpserver.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/applications", new IncidentHTTPHandler());
-        server.createContext("/applications", new RestaurantHTTPHandler());
+        server.createContext("/applications/incident", new IncidentHTTPHandler());
+        server.createContext("/applications/restaurant", new RestaurantHTTPHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
