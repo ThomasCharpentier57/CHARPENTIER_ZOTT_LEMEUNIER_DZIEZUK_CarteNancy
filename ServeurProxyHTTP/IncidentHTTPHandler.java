d7ad44574b80fba191604d2b9ea7f4a31558b46b
Alexis <120113186+KaiizokuOu@users.noreply.github.com>
2024-06-17 14:35:40 +0200

    finish merge proxyCentral

---
+++ ServeurProxyHTTP/IncidentHTTPHandler.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import com.sun.net.httpserver.*;
+
+
+public class IncidentHTTPHandler implements HttpHandler{
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+        try {
+            Registry regRestaurant = LocateRegistry.getRegistry();
+            regRestaurant.lookup("ServiceRestaurant");
+            String response = "This is the response";
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        } catch (RemoteException | NotBoundException e) {
+           e.printStackTrace();
+           exchange.sendResponseHeaders(500, -1);
+        }
+       
+    }
+    
+}
