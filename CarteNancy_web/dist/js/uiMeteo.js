cc3d53c706231143ca4f5eb577aa3bac642ef784
Alexis <120113186+KaiizokuOu@users.noreply.github.com>
2024-06-18 10:20:26 +0200

    fix merge

--- CarteNancy_web/dist/js/uiMeteo.js
+++ CarteNancy_web/dist/js/uiMeteo.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import { getDataTemp } from "./meteo.js";
-
-export async function displayMeteo() {
-    let data = await getDataTemp();
-    uiMeteo(data);
-}
-
-function uiMeteo(listeMeteo) {
-    let node = document.createElement("div");
-    document.querySelector("#content").appendChild(node);
-    node.setAttribute("id", "meteo");
-
-    const toStringMap = element => `
-        <div class="meteo-bubble">
-            <img class="meteo-icon" src="${element.icon}" alt="weather icon"/>
-            <div class="meteo-info">
-                <p class="meteo-date">${element.jour}</p>
-                <p class="meteo-time">${element.heure}</p>
-                <p class="meteo-temperature">Température: ${element.temperature}</p>
-                <p class="meteo-pluie">Pluie: ${element.pluie}</p>
-                <p class="meteo-vent">Vent: ${element.vent}</p>
-                <p class="meteo-nebulosite">Nébulosité: ${element.nebulosite}</p>
-            </div>
-        </div>
-    `;
-    let tabString = listeMeteo.map(toStringMap);
-    const concatReduce = (acc, element) => acc + element;
-    let result = tabString.reduce(concatReduce, "");
-    node.innerHTML = result;
-}
-=======
 import { getDataTemp } from "./meteo.js";
 
 export async function displayMeteo() {
@@ -59,4 +27,3 @@ function uiMeteo(listeMeteo) {
     let result = tabString.reduce(concatReduce, "");
     node.innerHTML = result;
 }
->>>>>>> 569193ff9685069421d748b8680a6dbad51be3e1
