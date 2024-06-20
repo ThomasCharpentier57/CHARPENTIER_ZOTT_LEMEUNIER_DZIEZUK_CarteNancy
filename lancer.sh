f7e051f05ece6e3c266fa47ebfc189524e34cac7
Alexis <120113186+alexiszott@users.noreply.github.com>
2024-06-20 14:40:57 +0200

    Update lancer.sh

--- lancer.sh
+++ lancer.sh
@@ -4,28 +4,18 @@ DEST_DIR="$(pwd)"
 
 cd "$DEST_DIR"
 
-clear
-
 cd ServiceProxyHTTP
 javac *.java
 rmiregistry 2000 &
 java LancerService &
-
-clear
 
 cd ../ServiceBDD/Service
 javac -cp .:ojdbc11.jar:json-20131018.jar *.java
 rmiregistry 1099  &
 java -cp .:ojdbc11.jar:json-20131018.jar LancerServiceRestaurant &
 
-clear
-
 cd ../../ServeurHTTP
 javac *.java
 java LancerServeurHTTP &
 
-clear
-
 xdg-open "https://webetu.iutnc.univ-lorraine.fr/~zott1u/CHARPENTIER_ZOTT_LEMEUNIER_DZIEZUK_CarteNancy-main/CarteNancy_web/"
-
-clear
